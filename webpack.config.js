57579a04d83fcf10f0aec80ea24fe9a5e017c238
ZMdog <zmdogg3@gmail.com>
2026-03-11 02:01:42 +0300

    мелочь

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html'
     }),
-    new Dotenv()
+    new Dotenv({
+    path: './.env'
+  })
   ],
   resolve: {
     extensions: [
